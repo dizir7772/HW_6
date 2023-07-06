7f84404576547cc938fbf959cd98cc38fa3471a5
Viacheslav Artemenko <v.a.artemenko1990@gmail.com>
2023-07-06 21:44:26 +0300

    script that sorts all files and folders

---
+++ normalize.py
@@ -0,0 +1,25 @@
+import re
+
+CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", "ji", "g")
+TRANS = {}
+for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper()
+
+
+def normalize(name: str) -> str:
+    t_name = name.translate(TRANS)
+    t_name = re.sub(r'\W', '_', t_name)
+    
+    return return_extension(t_name)
+
+
+def return_extension(string: str) -> str:
+    parts = string.rsplit('_', 1)
+    extension = parts[1]
+    new_string = parts[0] + "." + extension
+    
+    return new_string
+
